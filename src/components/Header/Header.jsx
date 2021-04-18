2e4e1b59398d654c7c3cacb5fea40f1b2231d8fe
danil.sechin <reymonsma@gmail.com>
2021-04-19 03:36:46 +0500

    Moved Login component to Header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,40 +1,57 @@
-import { useRef } from "react";
+import { useRef, useState } from "react";
 import { connect } from "react-redux";
 import { NavLink } from "react-router-dom"
 import { getLogin, isAuth } from "../../redux/selectors/authSelectors";
+import Login from "../Login/Login";
 import styles from "./Header.module.scss";
 
 const Header = ({ isAuth, login }) => {
   const menuLine = useRef();
   const list = useRef();
+  const body = useRef(document.body);
+
+  const [isFormOpen, setIsFormOpen] = useState(false);
 
   const toggleMenu = () => {
     menuLine.current.classList.toggle(styles.active);
     list.current.classList.toggle(styles.active);
   }
 
+  const openForm = () => {
+    setIsFormOpen(true);
+    body.current.style.overflow = "hidden";
+  }
+
+  const closeForm = () => {
+    setIsFormOpen(false);
+    body.current.style.overflow = "auto";
+  }
+
   return (
-    <header className={styles.header}>
-      <div className={styles.menuLineWrapper} onClick={toggleMenu}><div className={styles.menuLine} ref={menuLine}></div></div>
-      <ul className={styles.list} ref={list}>
-        <li className={styles.listItem}><NavLink className={styles.link} to="/">Главная</NavLink></li>
-        <li className={styles.listItem}><a className={styles.link} href="#anchor">Заказать</a></li>
-        <li className={styles.listItem}><NavLink className={styles.link} to="/cars">Список авто</NavLink></li>
-        <li className={styles.listItem}><NavLink className={styles.link} to="/support">Поддержка</NavLink></li>
-      </ul>
-      {
-        isAuth
-        ? <NavLink className={styles.link} to="/profile">{login}</NavLink>
-        : <a className={styles.link} href="#anchor">Войти</a>
-      }
-    </header>
+    <>
+      <Login isFormOpen={isFormOpen} closeForm={closeForm}/>
+      <header className={styles.header}>
+        <div className={styles.menuLineWrapper} onClick={toggleMenu}><div className={styles.menuLine} ref={menuLine}></div></div>
+        <ul className={styles.list} ref={list}>
+          <li className={styles.listItem}><NavLink className={styles.link} to="/">Главная</NavLink></li>
+          <li className={styles.listItem}><a className={styles.link} href="#anchor">Заказать</a></li>
+          <li className={styles.listItem}><NavLink className={styles.link} to="/cars">Список авто</NavLink></li>
+          <li className={styles.listItem}><NavLink className={styles.link} to="/support">Поддержка</NavLink></li>
+        </ul>
+        {
+          isAuth
+          ? <NavLink className={styles.link} to="/profile">{login}</NavLink>
+          : <span className={styles.link} onClick={openForm}>Войти</span>
+        }
+      </header>
+    </>
   )
 }
 
 const mapStateToProps = (state) => {
   return {
     isAuth: isAuth(state),
-    login: getLogin(state)
+    login: getLogin(state),
   }
 }
 
